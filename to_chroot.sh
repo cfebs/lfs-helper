a5a6f55375e289328436b69b3b409f445b267d02
Collin Lefeber <hello@cfebs.com>
2024-05-27 14:11:15 -0400

    to_chroot

---
+++ to_chroot.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+
+VERSION=12.1
+
+if [[ "$(id -u)" != "0" ]]; then
+	echo "ERROR: not using root user"
+	exit 1;
+fi
+
+if [[ -z "$LFS" ]]; then
+	echo "ERROR: empty LFS env var, exiting"
+	exit 1
+fi
+
+echo ">> Changing ownership of setup dirs"
+chown -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
+case $(uname -m) in
+  x86_64) chown -R root:root $LFS/lib64 ;;
+esac
+
+echo ">> Creating virtual fs dirs"
+mkdir -pv $LFS/{dev,proc,sys,run}
+
+echo ">> Mounting virtual dirs"
+mount -v --bind /dev $LFS/dev
+mount -vt devpts devpts -o gid=5,mode=0620 $LFS/dev/pts
+mount -vt proc proc $LFS/proc
+mount -vt sysfs sysfs $LFS/sys
+mount -vt tmpfs tmpfs $LFS/run
+if [ -h $LFS/dev/shm ]; then
+  install -v -d -m 1777 $LFS$(realpath /dev/shm)
+else
+  mount -vt tmpfs -o nosuid,nodev tmpfs $LFS/dev/shm
+fi
+
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+	MAKEFLAGS="-j$(( $(nproc) - 2 ))" \
+    TESTSUITEFLAGS="-j$(( $(nproc) - 2 ))" \
+    /bin/bash --login
